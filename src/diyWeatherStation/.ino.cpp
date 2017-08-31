a00b0f2d06baf1f7efcb15e661fe7bd4ad8767d2
rage5474 <raphael.geissler@gmail.com>
2017-08-31 21:55:29 +0200

    Generated stuff.

--- src/diyWeatherStation/.ino.cpp
+++ src/diyWeatherStation/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-08-31 21:47:52
+//This file has been generated on 2017-08-31 21:53:28
 
 #include "Arduino.h"
 #include "Arduino.h"
